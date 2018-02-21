7dfdce1391529db03f50e31fb6c584b7044d9ed3
snlan <snlan@live.cn>
2018-02-21 23:02:57 +0800

    f

--- microservice-provider-user/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-insert into user(id, username, name, age, balance) VALUES (1, 'user1', 'snlan1', 20, 100.00);
-insert into user(id, username, name, age, balance) VALUES (2, 'user2', 'snlan2', 20, 100.00);
-insert into user(id, username, name, age, balance) VALUES (3, 'user3', 'snlan3', 20, 100.00);
-insert into user(id, username, name, age, balance) VALUES (4, 'user4', 'snlan4', 20, 100.00);
