3a17b990193f67e35d074a3de715e6461f00df05
snlan <snlan@live.cn>
2018-02-25 21:36:22 +0800

    F

--- microservice-hystrix-turbine/src/main/java/com/itmuch/cloud/TurbineApplication.java
+++ microservice-hystrix-turbine/src/main/java/com/itmuch/cloud/TurbineApplication.java
@@ -1,10 +1,18 @@
 package com.itmuch.cloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableTurbine
 public class TurbineApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SpringApplication.run(TurbineApplication.class, args);
 	}
 
 }
